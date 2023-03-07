007a2e6cad5b6a9aa669a2528d792271b8ed881b
starryeye <see2818@naver.com>
2023-03-07 20:41:33 +0900

    [starryeye, 2023.03.07] - Order Gift Domain 개발
    내용
    - Order Gift 도메인 개발
    - 선물하기 결제 요청 처리 로직
    - 보상 트랜잭션 회피 방법 (순서 바꾸기)

---
+++ order/src/main/java/dev/practice/order/domain/order/gift/GiftOrderServiceImpl.java
@@ -0,0 +1,43 @@
+package dev.practice.order.domain.order.gift;
+
+import dev.practice.order.common.exception.IllegalStatusException;
+import dev.practice.order.domain.order.Order;
+import dev.practice.order.domain.order.OrderCommand;
+import dev.practice.order.domain.order.OrderReader;
+import dev.practice.order.domain.order.payment.PaymentProcessor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class GiftOrderServiceImpl implements GiftOrderService{
+
+    private final OrderReader orderReader;
+    private final PaymentProcessor paymentProcessor;
+
+    private final GiftMessageChannelSender giftMessageChannelSender;
+
+    @Override
+    @Transactional
+    public void paymentOrder(OrderCommand.PaymentRequest paymentRequest) {
+        log.info("GiftOrderService.paymentOrder request = {}", paymentRequest);
+
+        var order = orderReader.getOrder(paymentRequest.getOrderToken());
+
+        //orderComplete 와 중복 로직, 보상트랜잭션 회피 위함
+        if(order.getStatus() != Order.Status.INIT)
+            throw new IllegalStatusException();
+
+        //주문 결제 처리
+        paymentProcessor.pay(order, paymentRequest);
+
+        //주문 결제 완료
+        order.orderComplete();
+
+        //
+        giftMessageChannelSender.paymentComplete(new GiftPaymentCompleteMessage(order.getOrderToken()));
+    }
+}
