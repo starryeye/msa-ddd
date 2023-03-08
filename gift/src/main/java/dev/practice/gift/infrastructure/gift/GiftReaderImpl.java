926fad205e4b56a81b4e65521753deceea63fd79
starryeye <see2818@naver.com>
2023-03-09 00:48:30 +0900

    [starryeye, 2023.03.09] - Gift Infrastructure 개발
    내용
     - 기본 DB 연동 로직 개발
     - DB 등록, 조회

---
+++ gift/src/main/java/dev/practice/gift/infrastructure/gift/GiftReaderImpl.java
@@ -0,0 +1,38 @@
+package dev.practice.gift.infrastructure.gift;
+
+import dev.practice.gift.common.exception.EntityNotFoundException;
+import dev.practice.gift.common.exception.InvalidParamException;
+import dev.practice.gift.domain.gift.Gift;
+import dev.practice.gift.domain.gift.GiftReader;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class GiftReaderImpl implements GiftReader {
+
+    private final GiftRepository giftRepository;
+
+    @Override
+    public Gift getGiftBy(String giftToken) {
+
+        if(StringUtils.isBlank(giftToken))
+            throw new InvalidParamException("giftToken not exist.");
+
+        return giftRepository.findByGiftToken(giftToken)
+                .orElseThrow(EntityNotFoundException::new);
+    }
+
+    @Override
+    public Gift getGiftByOrderToken(String orderToken) {
+
+        if(StringUtils.isBlank(orderToken))
+            throw new InvalidParamException("giftToken not exist.");
+
+        return giftRepository.findByOrderToken(orderToken)
+                .orElseThrow(EntityNotFoundException::new);
+    }
+}
---
+++ gift/src/main/java/dev/practice/gift/infrastructure/gift/GiftStoreImpl.java
@@ -0,0 +1,24 @@
+package dev.practice.gift.infrastructure.gift;
+
+import dev.practice.gift.common.exception.InvalidParamException;
+import dev.practice.gift.domain.gift.Gift;
+import dev.practice.gift.domain.gift.GiftStore;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class GiftStoreImpl implements GiftStore {
+
+    private final GiftRepository giftRepository;
+
+    @Override
+    public Gift store(Gift gift) {
+
+        if(gift == null) throw new InvalidParamException("Gift not exist");
+
+        return giftRepository.save(gift);
+    }
+}
