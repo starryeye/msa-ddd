27b5e5f23e2c83adbc9970a919e2d5bc9109af40
starryeye <see2818@naver.com>
2023-03-16 21:10:36 +0900

    [starryeye, 2023.03.16] - Auditing 의존성 개선
    내용
    - hibernate 의존성에서 spring-data 의존성으로 변경

--- gift/src/main/java/dev/practice/gift/domain/AbstractEntity.java
+++ gift/src/main/java/dev/practice/gift/domain/AbstractEntity.java
@@ -1,10 +1,11 @@
 package dev.practice.gift.domain;
 
 import lombok.Getter;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import javax.persistence.Column;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import java.time.ZonedDateTime;
@@ -14,9 +15,10 @@ import java.time.ZonedDateTime;
 @EntityListeners(AuditingEntityListener.class)
 public class AbstractEntity {
 
-    @CreationTimestamp
+    @CreatedDate
+    @Column(updatable = false)
     private ZonedDateTime createdAt;
 
-    @UpdateTimestamp
+    @LastModifiedDate
     private ZonedDateTime updatedAt;
 }
